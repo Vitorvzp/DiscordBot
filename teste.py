b279584b159895aabe0ef8ab570ea85d1cf63d1c
Vitorvzp <166327340+Vitorvzp@users.noreply.github.com>
2025-05-28 19:41:54 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-# on_ready
-# await bot.change_presence(activity=(discord.Game('VsCode')), status=discord.Status.do_not_disturb)
-# await bot.tree.sync()
-# log.start()
-
-# @tasks.loop(time=time(15))
-# async def log():
-#   channel = bot.get_channel(1377402614404223076)
-#   await channel.send(file=discord.File(r'Logs\logEnters'))
