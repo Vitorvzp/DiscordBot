9cfd0ea4605289f72b80e71e2fe4167fbfb7c63b
Vitorvzp <166327340+Vitorvzp@users.noreply.github.com>
2025-05-29 18:31:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ async def desc(interact:discord.Interaction, message: str):
   await interact.response.send_message(functions.security.descriptografar(message), ephemeral=True)
 
 
+
 @bot.tree.command(name="start",description="Faz uma Request na API")
 @app_commands.describe(link="API Link")
 async def start(interact:discord.Interaction, link:str):
@@ -50,12 +51,20 @@ async def start(interact:discord.Interaction, link:str):
   api_embed = discord.Embed()
   api_embed.title = f"    {functions.connect.conectar_api(link)} às {date.hour}:{date.minute}:{date.second}"
   api_embed.set_image(url="https://images-ext-1.discordapp.net/external/HQu3K9GCV6VqQz9FtJ8lYRxdh8stSq8RPWW1QmaVR0o/https/i.pinimg.com/736x/94/4b/c8/944bc8639a85065beebddd72f6a33b64.jpg?format=webp")
-  file=discord.File(r'result/result.txt')
-  filer=discord.File(r'recived/recived.txt')
+  file=discord.File(r'result\result.txt')
+  filer=discord.File(r'recived\recived.txt')
   await interact.response.send_message(embed=api_embed)
   s(1)
   await interact.followup.send(file=file, ephemeral=True)
   await channel.send(file=filer)
+@start.autocomplete('link')
+async def complete(interact:discord.Interaction, recived:str):
+  links = ['https://api-c783.onrender.com/Usuarios', 'https://api-c783.onrender.com/Nomes']
+  options = []
+  for link in links:
+    option = app_commands.Choice(name=link, value=link)
+    options.append(option)
+  return options[:]
 
 @bot.command()
 async def online(ctx:commands.Context):
