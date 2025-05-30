4fa665a528b586504d97c63425bf7529acbc3216
Vitorvzp <166327340+Vitorvzp@users.noreply.github.com>
2025-05-29 21:42:24 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -25,38 +25,105 @@ async def on_ready():
   await channel.send(f'{len(sinc)} Comandos Sincronizados!', delete_after=5)
   print('✔ ON')
 
+@bot.event
+async def on_member_join(member: discord.Member):
+    list_users = [1038983303555977297]
+    user_id = member.id
+    guild = member.guild
+    viwer = discord.utils.get(guild.roles, id=1377349468902395904)
+    admin = discord.utils.get(guild.roles, id=1263291925386231990)
+    if user_id in list_users:
+        await member.add_roles(admin)
+    else:
+        await member.add_roles(viwer)
+
+@bot.tree.command(name='help', description='Mostra Todos os Comandos')
+async def ajuda(interact:discord.Interaction):
+   ch1 = bot.get_channel(1262107519686283305)
+   ch2 = bot.get_channel(1377355498852188250)
+   await interact.response.send_message(f"# EXEMPLOS:```/clear``````/criptografar Olá``````/descriptografar zMá```Usado em {ch1.mention}", ephemeral=True)
+   await interact.followup.send(f"# EXEMPLOS: ```/start https://api-c783.onrender.com/Usuarios``````/start https://api-c783.onrender.com/Nomes```Usado em {ch2.mention}", ephemeral=True)
+
 @bot.tree.command(name='clear', description='Limpa Todas as Mensagens do Canal!')
 async def clear(interact:discord.Interaction):
-  await interact.channel.purge()
+  channel = bot.get_channel(1377783988353241218)
+  roles = interact.user.roles
+  role = discord.utils.get(interact.guild.roles, id=1377302864350675024)
+  not_delete = [1376737654749528135, 1377368228535472190, 1377402614404223076, 1377783988353241218]
+  if role in roles and interact.channel_id not in not_delete:
+    await interact.channel.purge()
+    await channel.send(f'<@693587188650213432> {interact.user.mention} Apagou as Mensagens de {interact.channel.mention}')
+  elif interact.user.id == 693587188650213432 :
+    await interact.channel.purge()
+    await channel.send(f'<@693587188650213432> {interact.user.mention} Apagou as Mensagens de {interact.channel.mention}')
+  elif role not in roles:
+     await interact.response.send_message("Seu Cargo Não Tem Permissão Suficiente!", ephemeral=True)
+  elif interact.channel_id in not_delete:
+     await interact.response.send_message("As Mensagens Deste Canal Só Podem Ser Apagadas Pelo Dono!", ephemeral=True)
+  else:
+     await interact.response.send_message("ERRO!")
 
 @bot.tree.command(name='criptografar', description='Criptografa sua Mensagem!')
 @app_commands.describe(message="Digite Sua Mensagem!")
 async def crip(interact:discord.Interaction, message: str):
-  await interact.response.send_message(functions.security.criptografar(message), ephemeral=True)
-
+  channel = bot.get_channel(1377783988353241218)
+  mention = bot.get_channel(1262107519686283305)
+  roles = interact.user.roles
+  role = discord.utils.get(interact.guild.roles, id=1377302864350675024)
+  if role in roles and interact.channel_id == 1262107519686283305 or interact.user.id == 693587188650213432:
+    await interact.response.send_message(functions.security.criptografar(message), ephemeral=True)
+    await channel.send(f"<@693587188650213432>, {interact.user.mention} Criptografou a Mensagem: ```{message}``` (no canal {interact.channel.mention})")
+  elif role not in roles:
+     await interact.response.send_message("Seu Cargo Não Tem Permissão Suficiente!", ephemeral=True)
+  elif interact.channel_id != 1262107519686283305:
+     await interact.response.send_message(f"Neste Canal Não Fucina, Tente em {mention.mention}", ephemeral=True)
+  else:
+     await interact.response.send_message("ERRO!", ephemeral=True)
 
 @bot.tree.command(name='descriptografar', description='Desriptografa sua Mensagem!')
 @app_commands.describe(message="Digite Sua Mensagem!")
 async def desc(interact:discord.Interaction, message: str):
-  await interact.response.send_message(functions.security.descriptografar(message), ephemeral=True)
-
-
+  channel = bot.get_channel(1377783988353241218)
+  mention = bot.get_channel(1262107519686283305)
+  roles = interact.user.roles
+  role = discord.utils.get(interact.guild.roles, id=1377302864350675024)
+  if role in roles and interact.channel_id == 1262107519686283305 or interact.user.id == 693587188650213432:
+    await interact.response.send_message(functions.security.descriptografar(message), ephemeral=True)
+    await channel.send(f"<@693587188650213432>, {interact.user.mention} Desriptografou a Mensagem: ```{message}``` (no canal {interact.channel.mention})")
+  elif role not in roles:
+     await interact.response.send_message("Seu Cargo Não Tem Permissão Suficiente!", ephemeral=True)
+  elif interact.channel_id != 1262107519686283305:
+     await interact.response.send_message(f"Neste Canal Não Fucina, Tente em {mention.mention}", ephemeral=True)
+  else:
+     await interact.response.send_message("ERRO!", ephemeral=True)
 
 @bot.tree.command(name="start",description="Faz uma Request na API")
 @app_commands.describe(link="API Link")
 async def start(interact:discord.Interaction, link:str):
   channel = bot.get_channel(1377368228535472190)
-  date = datetime.now()
-
-  api_embed = discord.Embed()
-  api_embed.title = f"    {functions.connect.conectar_api(link)} às {date.hour}:{date.minute}:{date.second}"
-  api_embed.set_image(url="https://images-ext-1.discordapp.net/external/HQu3K9GCV6VqQz9FtJ8lYRxdh8stSq8RPWW1QmaVR0o/https/i.pinimg.com/736x/94/4b/c8/944bc8639a85065beebddd72f6a33b64.jpg?format=webp")
-  file=discord.File(r'result\result.txt')
-  filer=discord.File(r'recived\recived.txt')
-  await interact.response.send_message(embed=api_embed)
-  s(1)
-  await interact.followup.send(file=file, ephemeral=True)
-  await channel.send(file=filer)
+  mention = bot.get_channel(1262107519686283305)
+  roles = interact.user.roles
+  role = discord.utils.get(interact.guild.roles, id=1377302864350675024)
+  if role in roles and interact.channel_id == 1262107519686283305 or interact.user.id == 693587188650213432:
+    date = datetime.now()
+    api_embed = discord.Embed()
+    api_embed.title = f"    {functions.connect.conectar_api(link)} às {date.hour}:{date.minute}:{date.second}"
+    api_embed.set_image(url="https://images-ext-1.discordapp.net/external/HQu3K9GCV6VqQz9FtJ8lYRxdh8stSq8RPWW1QmaVR0o/https/i.pinimg.com/736x/94/4b/c8/944bc8639a85065beebddd72f6a33b64.jpg?format=webp")
+    file=discord.File(r'result\result.txt')
+    filer=discord.File(r'recived\recived.txt')
+    await interact.response.send_message(embed=api_embed, ephemeral=True)
+    s(1)
+    await interact.followup.send(file=file, ephemeral=True)
+    await channel.send(file=filer)
+    ch = bot.get_channel(1377783988353241218)
+    ch2 = bot.get_channel(1377368228535472190)
+    await ch.send(f"<@693587188650213432>, {interact.user.mention} usou /start no link ```{link}``` Verifique {ch2.mention}")
+  elif role not in roles:
+     await interact.response.send_message("Seu Cargo Não Tem Permissão Suficiente!", ephemeral=True)
+  elif interact.channel_id != 1262107519686283305:
+     await interact.response.send_message(f"Neste Canal Não Fucina, Tente em {mention.mention}", ephemeral=True)
+  else:
+     await interact.response.send_message("ERRO!", ephemeral=True)
 @start.autocomplete('link')
 async def complete(interact:discord.Interaction, recived:str):
   links = ['https://api-c783.onrender.com/Usuarios', 'https://api-c783.onrender.com/Nomes']
@@ -70,9 +137,6 @@ async def complete(interact:discord.Interaction, recived:str):
 async def online(ctx:commands.Context):
   user = 693587188650213432
   mention = f"<@{user}>"
-  if ctx.author.bot:
-    await ctx.send(f'{mention} Tô on, Malucão!')
-  else:
-    await ctx.reply("Online!")
+  await ctx.reply(f'{mention} Tô on Malucão!')
 
 bot.run(Token)
